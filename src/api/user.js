80cbbcf136ec91af3b50dbf060ce1da49c01bf4f
Ayaka Neko <neko@ayaka.moe>
2020-07-07 19:49:49 +0800

    Added the avatar endpoint to retrive a avatar
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/user.js
+++ src/api/user.js
@@ -3,11 +3,13 @@ const KoaRouter = require('koa-router')
 
 // Local Dependencies
 const login = require('../controllers/login')
+const getAvatar = require('../controllers/avatar')
 const { avatar, nickname, email } = require('../controllers/update')
 
 let user = new KoaRouter()
 
 user.get("/login", login)
+user.get("/avatar", getAvatar)
 user.get("/update/email", email)
 user.get("/update/avatar", avatar)
 user.get("/update/nickname", nickname)
