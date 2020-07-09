a9601fd6a2d04d5a0814c278a5b00664de0dfbac
Ayaka Neko <neko@ayaka.moe>
2020-07-09 22:38:58 +0800

    Fixed a issue that /:notifyIdy may catch up the request before /push
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/notification.js
+++ src/api/notification.js
@@ -1,12 +1,11 @@
 const KoaRouter = require('koa-router')
 
-const { notifyUpdate, notifyNew, notifyObject, notifyPush } = require('../controllers/notification')
+const { notifyUpdate, notifyNew, notifyPush } = require('../controllers/notification')
 
 let notificationRouter = new KoaRouter()
 
 notificationRouter.get("/update", notifyUpdate)
 notificationRouter.get("/push", notifyPush)
 notificationRouter.get("/new", notifyNew)
-notificationRouter.get("/:notifyId", notifyObject)
 
 module.exports = notificationRouter
