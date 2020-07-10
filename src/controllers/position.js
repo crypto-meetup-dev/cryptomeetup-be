192753863b27dfeb83ad00dbc88661989de31c2c
Ayaka Neko <neko@ayaka.moe>
2020-07-10 16:48:28 +0800

    Added the position endpoint of the query user
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/position.js
+++ src/controllers/position.js
@@ -1,8 +1,14 @@
+// Local Packages
+const Log = require('../util/log')
+const Store = require('../store/store')
 
 let position = async (ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
-    ctx.body = query
+    let user = await Store.user.findOne({ key: "UserProfile", id: query.id })
+    console.log(user)
+    let position = [user.lng, user.lat]
+    ctx.body = position
 }
 
 module.exports = position
