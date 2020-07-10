5e52908a4ffd079bdbabfe7be30d207f238fb54f
Ayaka Neko <neko@ayaka.moe>
2020-07-10 17:02:18 +0800

    Added the friends endpoint
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -1,7 +1,12 @@
+// Local Packages
+const Log = require('../util/log')
+const Store = require('../store/store')
+
 let friends = async(ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
-    ctx.body = query
+    let friends = await Store.user.findOne({ key: "FriendsProfile", id: query.id })
+    ctx.body = friends.users
 }
 
 let friendsUpdate = async (ctx, next) => {
