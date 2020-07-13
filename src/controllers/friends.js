edc5733fd06cf40d6c036b8fb524fbc388e97e25
Ayaka Neko <neko@ayaka.moe>
2020-07-13 17:26:04 +0800

    Added assert break at friends endpoint
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -6,9 +6,15 @@ const user = require('../api/user')
 let friends = async(ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
+
+    if (query.id === undefined || query.id === "undefined" || query.id === "null") {
+        ctx.body = { status: "failed", message: "Invalid Request, Missing value on required field `id`" }
+        await next()
+        return
+    }
+
     let friends = await Store.user.findOne({ key: "FriendsProfile", id: query.id })
     let friendObjects = new Array()
-
     for(let i = 0; i < friends.users.length; i++) {
         let friend = await Store.user.findOne({ key: "UserProfile", id: friends.users[i] })
         let friendObject = {
