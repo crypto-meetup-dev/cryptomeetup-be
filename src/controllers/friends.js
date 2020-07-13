db05d6061e54a21742c5bd0cb8183c2fecd92555
Ayaka Neko <neko@ayaka.moe>
2020-07-13 19:50:48 +0800

    Added dismiss friend endpoint
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -33,7 +33,25 @@ let friends = async(ctx, next) => {
 let friendsUpdate = async (ctx, next) => {
     let query = ctx.request.query
     query = JSON.parse(JSON.stringify(query))
-    ctx.body = query
+    
+    if (query.id === undefined || query.id === "undefined" || query.id === "null") {
+        ctx.body = { status: "failed", message: "Invalid Request, Missing value on required field `id`" }
+        await next()
+        return
+    }
+    if (query.removeId === undefined || query.id === "undefined" || query.id === "null") {
+        ctx.body = { status: "failed", message: "Invalid Request, Missing value on required field `id`" }
+        await next()
+        return
+    }
+
+    let friends = await Store.user.findOne({ key: "FriendsProfile", id: query.id })
+    friends = friends.users.filter(e => e.id !== parseInt(query.removeId))
+    await Store.user.update({ key: "FriendsProfile", id: query.id }, { $set: { users: friends } }, {})
+    
+    ctx.body = { message: "success" }
+    
+    await next()
 }
 
 module.exports = {
