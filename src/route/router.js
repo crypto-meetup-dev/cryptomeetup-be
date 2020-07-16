83edf93f7957f99ad90a708d44420bbbe7b75ae9
Ayaka Neko <neko@ayaka.moe>
2020-07-16 20:15:51 +0800

    Added subscribe api endpoints
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/route/router.js
+++ src/route/router.js
@@ -6,9 +6,11 @@ let user = require('../api/user')
 let inviteRouter = require('../api/invite')
 let friendsRouter = require('../api/friends')
 let notification = require('../api/notification')
+let subscribeRouter = require('../api/subscribe')
 
 routers.use("/user", user.routes(), user.allowedMethods())
 routers.use("/invite", inviteRouter.routes(), inviteRouter.allowedMethods())
+routers.use("/subscribe", subscribeRouter.routes(), subscribeRouter.allowedMethods())
 routers.use("/friends", friendsRouter.routes(), friendsRouter.allowedMethods())
 routers.use("/notification", notification.routes(), notification.allowedMethods())
 
